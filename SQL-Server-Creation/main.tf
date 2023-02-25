63cad7ec4da3d673d6f41d686d5a0d235fe3c43d
steveramos21@gmail.com <steveramos21@gmail.com>
2023-02-25 11:16:37 +0800

    Adding SQL-Server-Creation

---
+++ SQL-Server-Creation/main.tf
@@ -0,0 +1,42 @@
+# To run the Terraform config on localhost with the secrets:
+# terraform plan -var-file="secrets.tfvars"
+
+provider "azurerm" {
+  features {}
+}
+
+#Creates resource group
+resource "azurerm_resource_group" "devopsdatabase" {
+  name     = var.rg_name
+  location = var.location
+}
+
+
+resource "azurerm_mysql_server" "devopsdatabase" {
+  name                = var.db_name
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  administrator_login          = "mysqladmin"
+  administrator_login_password = var.password
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "8.0"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+}
+
+
+resource "azurerm_mysql_firewall_rule" "firewallrule" {
+  name                = "office"
+  resource_group_name = azurerm_mysql_server.devopsdatabase.resource_group_name
+  server_name         = azurerm_mysql_server.devopsdatabase.name
+  start_ip_address    = var.ip_address
+  end_ip_address      = var.ip_address
+}
---
+++ SQL-Server-Creation/terraform.tfvars
@@ -0,0 +1,3 @@
+db_name = "mydatabasedevops19292"
+rg_name = "cloudskills-rg"
+location = "eastus"
